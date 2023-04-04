0428ff675feadb2ce968aaa4482183d8595da1f9
Sokhib <127226435+smash3-11@users.noreply.github.com>
2023-04-05 01:48:31 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,19 @@
 // Задание 1
-let index = +prompt()
+let index = +prompt('Введите индекс')
 let arr = ['Alexsey', 'Kartoshka', 'Margarita', 'Morgenshtern', 'Monica']
 if (arr.includes(arr[index])) {
-    arr.splice(index, 0);
-    console.log(`Элемент ${arr[index]} удален из массива`);
+    arr.splice(index, 1);
+    console.log(arr);
+    // console.log(`Элемент ${arr[index]} удален из массива`);
 } else {
     console.log(`Элемент с индексом ${index} отсутствует в массиве`);
 }
 
-// Задание 2
-
-// let arr_1 = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
-
-
-// if (arr_1.typeof(arr_1) >= Array(5)) {
-//     console.log("Good");
-// } else {
-//     console.log("bad");    
-// }
-// // console.log(typeof(arr_1));
-// // typeof(arr_1) >= [+prompt()]
-// // console.log(Array);
 // Задание 3
 
 
-// let arr_1 = [[{a :{price: 20}}][{a: {price: 35}}][{a: {price: 44}}]]
-// console.log(arr_1);
+let arr_1 = [[{a: {price: 20}}], [{a: {price: 35}}], [{a: {price: 44}}]];
+
+let total = [arr_1 [0][0].a.price + arr_1 [1][0].a.price + arr_1 [2][0].a.price ]
+
+console.log(total);
